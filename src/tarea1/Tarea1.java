28ea0597b39ccf0d223e46586489258235368419
CHCAJI <CHCAJI@192.168.1.54>
2016-07-21 22:55:45 -0600

    second commit

--- src/tarea1/Tarea1.java
+++ src/tarea1/Tarea1.java
@@ -17,6 +17,7 @@ public class Tarea1 {
     public static void main(String[] args) {
     
         System.out.println("Hello putitos");
+        System.out.println("Vamoadescontrolarno");
     }
     
 }
